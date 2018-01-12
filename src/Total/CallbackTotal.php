f9de3455177add05d3fb1dedbd848f9f5abcc8d8
andrey.koposov <andrey.koposov@merkeleon.com>
2018-01-12 15:19:23 +0300

    callback total

---
+++ src/Total/CallbackTotal.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrey
+ * Date: 07.11.16
+ * Time: 15:24
+ */
+
+namespace Merkeleon\Table\Total;
+
+use Merkeleon\Table\Total;
+use Closure;
+
+class CallbackTotal extends Total
+{
+    /** @var Closure $callback */
+    protected $callback = null;
+
+    public function get($model)
+    {
+        if (is_callable($this->callback))
+        {
+            $this->callback->call($this, $model);
+        }
+
+        return $model;
+    }
+
+    public function setCallback(Closure $callback)
+    {
+        $this->callback = $callback;
+
+        return $this;
+    }
+}
