fee702b6a538242edabcbf6ede0385d0d98188e5
Andrey Drozd <andrey@drozd.by>
2018-02-08 13:18:18 +0300

    improve select filter (allow filter by selection)

--- src/Filter/SelectFilter.php
+++ src/Filter/SelectFilter.php
@@ -8,31 +8,53 @@ use Merkeleon\Table\Filter;
 class SelectFilter extends Filter
 {
 
-    protected $options = [];
+    protected $options  = [];
     protected $viewPath = 'filters.select';
+
+    public function callbackFilter($model, $relations)
+    {
+        if (count($relations) == 1)
+        {
+            $field = array_first($relations);
+            $model->where($field, '=', $this->value);
+        }
+        else
+        {
+            $relation = array_shift($relations);
+            return $model->whereHas($relation, function($query) use ($relations)
+            {
+                return $this->callbackFilter($query, $relations);
+            });
+        }
+    }
 
     public function applyFilter($model)
     {
-        if ($this->value) {
-            $model = $model->where($model->getModel()->getTable().'.'.$this->name, $this->value);
+        if ($this->value)
+        {
+            $relations = explode('.', $this->name);
+            $this->callbackFilter($model, $relations);
         }
+
         return $model;
     }
 
     protected function prepare()
     {
-        $this->value = request('f_' . $this->name);
+        $this->value = request()->input('f_' . $this->preparedName());
     }
 
     public function options($options)
     {
         $this->options = $options;
+
         return $this;
     }
 
     public function render()
     {
         $view = parent::render();
+
         return $view->with('options', $this->options);
     }
 }
