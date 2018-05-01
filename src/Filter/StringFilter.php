cccc9c90c07e3d420d0661af2372981100bb6e71
andrei.korsak <codeator@gmail.com>
2018-05-01 21:16:13 +0300

    Casts to string filter

--- src/Filter/StringFilter.php
+++ src/Filter/StringFilter.php
@@ -10,12 +10,18 @@ class StringFilter extends Filter
 
     protected $viewPath = 'filters.string';
     protected $isStrict = false;
+    protected $cast     = false;
 
     public function params($params)
     {
         if (($isStrict = array_get($params, 'strict')))
         {
             $this->isStrict = $isStrict;
+        }
+
+        if (($cast = array_get($params, 'cast')))
+        {
+            $this->cast = $cast;
         }
 
         return parent::params($params);
@@ -30,13 +36,28 @@ class StringFilter extends Filter
     {
         if ($this->value)
         {
+            if ($this->cast)
+            {
+                if (in_array($this->cast, ['int', 'integer']))
+                {
+                    $this->value = (int)$this->value;
+                }
+
+                if (in_array($this->cast, ['str', 'string']))
+                {
+                    $this->value = (string)$this->value;
+                }
+            }
+
             if ($this->isStrict)
             {
-                $model = $model->where($model->getModel()->getTable() . '.' . $this->name, '=', $this->value);
+                $model = $model->where($model->getModel()
+                                             ->getTable() . '.' . $this->name, '=', $this->value);
             }
             else
             {
-                $model = $model->where($model->getModel()->getTable() . '.' . $this->name, 'like', '%' . $this->value . '%');
+                $model = $model->where($model->getModel()
+                                             ->getTable() . '.' . $this->name, 'like', '%' . $this->value . '%');
             }
         }
 
