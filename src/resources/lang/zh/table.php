7368bf8feaa4398117abf4c39216fbcf608bb4cf
Andrey Drozd <andrey@drozd.by>
2018-06-07 10:14:47 +0300

    add china (zh)

---
+++ src/resources/lang/zh/table.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'batch.action.selected' => '选定项目',
+    'batch.action.all'      => '全部项目',
+    'batch.labels.delete'   => '删除',
+    'button.filter'         => '筛选',
+    'button.reset'          => '重置',
+    'button.export-to'      => '导出到：格式',
+    'button.batch'          => '批量操作',
+    'filter.date.from'      => '从',
+    'filter.date.to'        => '至',
+    'filter.range.from'     => '从',
+    'filter.range.to'       => '至',
+    'pagination.from'       => '从',
+    'pagination.records'    => '记录',
+    'total.sum'             => '（总和）',
+    'total.count'           => '（计数）',
+    'total.callback'        => '',
+];
