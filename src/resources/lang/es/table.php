a549f745aac132d5e6a8bc186deddedc000e74fd
Eugene <eugene.ratkevich@gmail.com>
2018-12-13 14:40:33 +0300

    add new translates: es, id

---
+++ src/resources/lang/es/table.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'batch.action.selected' => 'Con seleccionados',
+    'batch.action.all'      => 'Con todos',
+    'batch.labels.delete'   => 'Eliminar',
+    'button.filter'         => 'Filtrar',
+    'button.reset'          => 'Resetear',
+    'button.export-to'      => 'Exportar a :format',
+    'button.batch'          => 'Lote',
+    'filter.date.from'      => 'De',
+    'filter.date.to'        => 'Hasta',
+    'filter.range.from'     => 'De',
+    'filter.range.to'       => 'A',
+    'pagination.from'       => 'de',
+    'pagination.records'    => 'registros',
+    'total.sum'             => '(monto total)',
+    'total.count'           => '(cantidad)',
+    'total.callback'        => '',
+    'row.empty'             => 'No hay datos',
+];
---
+++ src/resources/lang/id/table.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'batch.action.selected' => 'Dengan Pilihan Saja',
+    'batch.action.all'      => 'Dengan semua',
+    'batch.labels.delete'   => 'Hapus',
+    'button.filter'         => 'Filter',
+    'button.reset'          => 'Atur ulang',
+    'button.export-to'      => 'Ekspor ke :format',
+    'button.batch'          => 'Kelompok',
+    'filter.date.from'      => 'Dari',
+    'filter.date.to'        => 'Untuk',
+    'filter.range.from'     => 'Dari',
+    'filter.range.to'       => 'Untuk',
+    'pagination.from'       => 'dari',
+    'pagination.records'    => 'catatan',
+    'total.sum'             => '(jumlah)',
+    'total.count'           => '(hitung)',
+    'total.callback'        => '',
+    'row.empty'             => 'Tidak ada data',
+];
