af793ef7793336f9fee1cd8aa3d11d6aa8564dd0
Svetlana Temnikova <svetlana.temnikova@gmail.com>
2019-11-21 12:46:48 +0300

    add turkish language

---
+++ src/resources/lang/tr/table.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'batch.action.selected' => 'Seçililer ile',
+    'batch.action.all'      => 'Tümü ile',
+    'batch.labels.delete'   => 'Sil',
+    'button.filter'         => 'Filtrele',
+    'button.reset'          => 'Sıfırla',
+    'button.export-to'      => ':fomat \'a Aktar',
+    'button.batch'          => 'Toplu işlemler',
+    'filter.date.from'      => 'Başlangıç',
+    'filter.date.to'        => 'Bitiş',
+    'filter.range.from'     => 'Başlangıç',
+    'filter.range.to'       => 'Bitiş',
+    'pagination.from'       => 'başlangıç',
+    'pagination.records'    => 'kayıtlar',
+    'total.sum'             => '(toplam)',
+    'total.count'           => '(Sayı)',
+    'total.callback'        => '',
+    'row.empty'             => 'Veri yok',
+];
